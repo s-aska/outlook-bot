9154c5f35f603f1386de45cca766bdd6b2822246
Shinichiro Aska <s.aska.org@gmail.com>
2017-11-25 00:36:44 +0900

    親切エラー

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-console.log("TENANT_ID:", process.env.TENANT_ID);
-console.log("APPLICATION_ID:", process.env.APPLICATION_ID);
-
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const randomstring = require("randomstring");
 const request = require("request");
 
 const aud = "https://login.microsoftonline.com/" + process.env.TENANT_ID + "/oauth2/token";
-
 const x5t = fs.readFileSync("x5t.txt", "utf-8").trim();
-console.log("x5t:", x5t);
+const cert = fs.readFileSync("server.key");
 
-const header = { alg: "RS256", x5t: x5t };
-const payload = {
-  "aud": aud,
-  "iss": process.env.APPLICATION_ID,
-  "sub": process.env.APPLICATION_ID,
-  "jti": randomstring.generate(36),
-  "nbf": parseInt((new Date()).getTime() / 1000, 10),
-  "exp": parseInt((new Date()).getTime() / 1000, 10) + 3600
+const header = {
+  alg: "RS256",
+  x5t: x5t
 };
-const cert = fs.readFileSync("server.key");  // get private key
+const payload = {
+  aud: aud,
+  iss: process.env.APPLICATION_ID,
+  sub: process.env.APPLICATION_ID,
+  jti: randomstring.generate(36),
+  nbf: parseInt((new Date()).getTime() / 1000, 10),
+  exp: parseInt((new Date()).getTime() / 1000, 10) + 3600
+};
 const token = jwt.sign(payload, cert, { algorithm: "RS256", header: header });
 
-console.log("TOKEN:", token);
+console.log("TENANT_ID:", process.env.TENANT_ID);
+console.log("APPLICATION_ID:", process.env.APPLICATION_ID);
 
 let form = {
   grant_type: "client_credentials",
@@ -35,6 +34,7 @@ let form = {
 
 request.post({url: aud, form: form}, function (error, response, body) {
   if (error || response.statusCode != 200) {
+    console.log("TOKEN:", token);
     console.log("error:", error);
     console.log("statusCode:", response && response.statusCode);
     console.log("body:", body);
@@ -52,6 +52,7 @@ request.post({url: aud, form: form}, function (error, response, body) {
     }
   }, function (error, response, body) {
     if (error || response.statusCode != 200) {
+      console.log("Azure Active Directory 管理センター のアプリ登録画面、必要なアクセス許可メニューで「アクセス許可の付与」して下さい。");
       console.log("error:", error);
       console.log("statusCode:", response && response.statusCode);
       console.log("body:", body);
